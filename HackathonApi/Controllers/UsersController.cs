c9c7d670915debb10bd2c30ed98c833395f2e4a1
Hiten Soni <hiten.soni@clariontechnologies.co.in>
2020-05-29 11:32:06 +0530

    Updates

---
+++ HackathonApi/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HackathonApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public int Get()
+        {
+            var rng = new Random();
+            return rng.Next();
+        }
+    }
+}
