586f0fb8004eb51a42c2d85ed106b001c4f3c3a6
Hiten Soni <hiten.soni@clariontechnologies.co.in>
2020-05-29 22:47:54 +0530

    FIx

--- HackathonApi/Controllers/AccountController.cs
+++ HackathonApi/Controllers/AccountController.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using HackathonApi.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Auth0.ManagementApi;
+using Auth0.ManagementApi.Models;
 
 namespace HackathonApi.Controllers
 {
@@ -13,28 +20,50 @@ namespace HackathonApi.Controllers
     public class AccountController : ControllerBase
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly IConfiguration _configuration;
+        private CreateAccountRequestModel createAccountRequestModel { get; set; }
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
+
+          
         }
 
         [HttpPost]
         public CreateAccountResponseModel Post(CreateAccountRequestModel model)
         {
-            CreateAccountResponseModel response = null;
+            CreateAccountResponseModel responseModel = null;
+           
             try
             {
+              
 
+                string token = _configuration.GetSection("Auth0").GetSection("Token").Value;
+                string url = _configuration.GetSection("Auth0").GetSection("Url").Value;
+                url += "users";
+                using (var client = new ManagementApiClient(token, new Uri(url)))
+                {
+                    var response = client.Users.CreateAsync(new UserCreateRequest
+                    {
+                        Connection = "Username-Password-Authentication",
+                        Email = model.Email,
+                        Password = model.Password,
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                    }).Result;
+ 
+                }
             }
             catch (Exception ex)
             {
-                response.IsSuccess = false;
-                response.ErrorMessage = "Failed to create new account";
+                responseModel.IsSuccess = false;
+                responseModel.ErrorMessage = "Failed to create new account";
                 _logger.LogError(ex.ToString());
             }
 
-            return response;
+            return responseModel;
         }
     }
 }
--- HackathonApi/Model/CreateAccountRequestModel.cs
+++ HackathonApi/Model/CreateAccountRequestModel.cs
@@ -1,16 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace HackathonApi.Model
 {
     public class CreateAccountRequestModel
     {
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public string Email { get; set; }
+
+        public string Password { get; set; }
         public string DeviceId { get; set; }
-       
+
+ 
     }
 }
